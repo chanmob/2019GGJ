31282aec5f0296d9f081182dfbb08cafddb54867
Chanmob <sellingout@naver.com>
2019-01-26 00:59:08 +0900

    걷기, 돌 던지기 완료

--- 2019GGJ/Assets/Scripts/Player.cs
+++ 2019GGJ/Assets/Scripts/Player.cs
@@ -6,28 +6,117 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb2d;
 
+    private Vector3? pos;
     private Vector3 mousePos;
+    private Vector3 diff;
+
+    public Sprite[] stepSprites = new Sprite[3];
+
+    private SpriteRenderer spriteRenderer;
 
     public float speed;
+    private float rot_z;
+
+    private bool leftStep = false;
+
+    public int hp;
+
+    public GameObject stone;
+
+    private IEnumerator stepCoroutine;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("이동 시작");
+            rb2d.velocity = new Vector2(0, 0);
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            LookAt();
+            if(stepCoroutine == null)
+            {
+                stepCoroutine = ChangeFootStep();
+                StartCoroutine(stepCoroutine);
+            }
+            pos = mousePos;
+        }
+
+        if(pos != null)
+        {
             Vector2 direction = (mousePos - this.transform.position).normalized;
             rb2d.AddForce(direction * speed * Time.deltaTime);
-
-            if(Vector2.Distance(this.transform.position, mousePos) < 0.2f)
-            {
-                rb2d.velocity = new Vector2(0, 0);
-            }
         }
-	}
+
+        if (Vector2.Distance(this.transform.position, mousePos) < 0.1f)
+        {
+            StepStop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ThrowStone();
+        }
+    }
+
+    public void StepStop()
+    {
+        Debug.Log("정지");
+        pos = null;
+        rb2d.velocity = new Vector2(0, 0);
+        if (stepCoroutine != null)
+        {
+            StopCoroutine(stepCoroutine);
+            stepCoroutine = null;
+        }
+
+        spriteRenderer.sprite = stepSprites[0];
+    }
+
+    private IEnumerator ChangeFootStep()
+    {
+        while (true)
+        {
+            leftStep = !leftStep;
+
+            if (leftStep)
+            {
+                spriteRenderer.sprite = stepSprites[1];
+            }
+            else
+            {
+                spriteRenderer.sprite = stepSprites[2];
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    private void LookAt()
+    {
+        diff = mousePos - transform.position;
+        diff.Normalize();
+
+        rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
+    }
+
+    public void ThrowStone()
+    {
+        Debug.Log("돌을 던진다");
+        Instantiate(stone, this.transform.position, this.transform.rotation);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StepStop();
+    }
 }
---
+++ 2019GGJ/Assets/Scripts/Stone.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private CircleCollider2D circle2d;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        circle2d = GetComponent<CircleCollider2D>();
+        rb2d.AddForce(transform.up * -speed);
+
+        StartCoroutine(Wave());
+    }
+
+    private IEnumerator Wave()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("파동 시작");
+        rb2d.velocity = new Vector2(0, 0);
+        GetComponent<MeshRenderer>().enabled = false;
+
+        float t = 0.5f;
+
+        while (t < 2)
+        {
+            t += (Time.deltaTime / 1);
+
+            circle2d.radius = t;
+
+            yield return null;
+        }
+
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}
