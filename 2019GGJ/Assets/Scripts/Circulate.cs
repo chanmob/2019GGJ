ca76356522fe0babb8d6b0257bf05128bd7d1c89
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-01-26 03:41:45 +0900

    Complet UI
    UI 완성!

---
+++ 2019GGJ/Assets/Scripts/Circulate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Circulate : MonoBehaviour {
+
+	public float angularSpeed;
+	public Image img;
+	public GameObject gameover;
+	public GameObject settingBtn;
+	private float rotationAngle;
+
+
+	private void FixedUpdate()
+	{
+		rotationAngle = transform.eulerAngles.z;
+
+		if (rotationAngle > 265)
+		{
+			Debug.Log("player die");
+			StartCoroutine(FadeImage(false));
+		}
+		else
+		{
+			transform.Rotate(0, 0, Time.deltaTime + angularSpeed);
+		}
+	}
+	
+
+	IEnumerator FadeImage(bool fadeAway)
+	{
+		if (fadeAway)
+		{
+			for (float i = 1; i >= 0; i -= Time.deltaTime)
+			{
+				img.color = new Color(0, 0, 0, i);
+				yield return null;
+			}
+		}
+		else
+		{
+			for (float i = 0; i <= 1.1f; i += Time.deltaTime)
+			{
+				img.color = new Color(0, 0, 0, i);
+				yield return null;
+			}
+			Time.timeScale = 0;
+			settingBtn.SetActive(false);
+			gameover.SetActive(true);
+		}
+	}
+}
---
+++ 2019GGJ/Assets/Scripts/UIButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtonManager : MonoBehaviour {
+
+	public GameObject settingPanel;
+
+	public void PressRestartBtn()
+	{
+		Time.timeScale = 1;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void PressMainBtn()
+	{
+		SceneManager.LoadScene("Main");
+	}
+
+	public void PressSettingBtn()
+	{
+		settingPanel.SetActive(true);
+		Time.timeScale = 0;
+	}
+
+	public void PressExitSettingBtn()
+	{
+		settingPanel.SetActive(false);
+		Time.timeScale = 1;
+	}
+}
