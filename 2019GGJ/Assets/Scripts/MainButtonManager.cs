bb641fc8661c14078e56fcb8d035724e11d9139a
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-01-26 05:52:57 +0900

    Complete Main Scene
    Main 씬 UI 완성!

---
+++ 2019GGJ/Assets/Scripts/MainButtonManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtonManager : MonoBehaviour {
+
+	public GameObject saveDataPanel;
+	public GameObject settingPanel;
+	public GameObject creditsPanel;
+	public GameObject exitPanel;
+
+	public void PressGameStartBtn()
+	{
+		saveDataPanel.SetActive(true);
+	}
+
+	public void PressSettingsBtn()
+	{
+		settingPanel.SetActive(true);
+	}
+
+	public void PressCreditstBtn()
+	{
+		creditsPanel.SetActive(true);
+	}
+
+	public void PressExitBtn()
+	{
+		exitPanel.SetActive(true);
+	}
+
+	public void PressExitGameStartBtn()
+	{
+		saveDataPanel.SetActive(false);
+	}
+
+	public void PressExitSettingsBtn()
+	{
+		settingPanel.SetActive(false);
+	}
+
+	public void PressExitCreditsBtn()
+	{
+		creditsPanel.SetActive(false);
+	}
+
+	public void PressYesBtn()
+	{
+		Application.Quit();
+	}
+
+	public void PressNoBtn()
+	{
+		exitPanel.SetActive(false);
+	}
+}
