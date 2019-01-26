c325121c3134b6b04a7dcaa9d9916843dec21999
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-01-26 14:44:45 +0900

    Add object and Stage clear
    Stage A에 오브젝트 추가
    스테이지 클리어 구현

---
+++ 2019GGJ/Assets/Scripts/WayoutScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WayoutScript : MonoBehaviour {
+
+	public Image img;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.CompareTag("Player"))
+		{
+			StartCoroutine(FadeImage(false));
+		}
+	}
+
+	IEnumerator FadeImage(bool fadeAway)
+	{
+		if (fadeAway)
+		{
+			for (float i = 1; i >= 0; i -= Time.deltaTime)
+			{
+				img.color = new Color(0, 0, 0, i);
+				yield return null;
+			}
+		}
+		else
+		{
+			for (float i = 0; i <= 1.1f; i += Time.deltaTime)
+			{
+				img.color = new Color(0, 0, 0, i);
+				yield return null;
+			}
+
+			SceneManager.LoadScene("Ending");
+		}
+	}
+}
