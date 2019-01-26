88b0387613754e411f8566345794cf2bce7aba73
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-01-27 00:16:32 +0900

    firefly particle, first tutorial
    1. 반딧불이 파티클 프리펩
    2. 첫번째 튜토리얼 제작

--- 2019GGJ/Assets/Scripts/Dialogue/DialogueManager.cs
+++ 2019GGJ/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour {
 
@@ -9,8 +10,11 @@ public class DialogueManager : MonoBehaviour {
 	public float typeDelay;
 
 	public Text dialogueText;
+	public Text guideText;
 	public GameObject dialogueTrigger;
 	public GameObject player;
+	public Image img;
+	public string nextTutorial;
 
 	private Queue<string> sentences;
 	private string dialogue;
@@ -26,9 +30,14 @@ public class DialogueManager : MonoBehaviour {
 	{
 		if (Input.GetMouseButtonDown(0)&&dialogueEnd)
 		{
-			StartCoroutine(FadeText(true));
+			StartCoroutine(FadeText(dialogueText, true));
+			StartCoroutine(FadeText(guideText, false));
+			StartCoroutine(LoadNextTutorial(10, nextTutorial));
 			dialogueEnd = false;
 		}
+
+
+
 	}
 
 	public void StartDialogue(Dialogue dialogue)
@@ -83,13 +92,13 @@ public class DialogueManager : MonoBehaviour {
 		StartCoroutine(FadePlayer(false));
 	}
 
-	IEnumerator FadeText(bool fadeAway)
+	IEnumerator FadeText(Text text, bool fadeAway)
 	{
 		if (fadeAway)
 		{
 			for (float i = 1; i >= 0; i -= Time.deltaTime)
 			{
-				dialogueText.color = new Color(i, i, i, i); //까맣게 투명하게
+				text.color = new Color(i, i, i, i); //까맣게 투명하게
 				yield return null;
 			}
 		}
@@ -97,7 +106,7 @@ public class DialogueManager : MonoBehaviour {
 		{
 			for (float i = 0; i <= 1.1f; i += Time.deltaTime)
 			{
-				dialogueText.color = new Color(i, i, i, i); // 하얗게 not투명하게
+				text.color = new Color(i, i, i, i); // 하얗게 not투명하게
 				yield return null;
 			}
 		}
@@ -121,5 +130,21 @@ public class DialogueManager : MonoBehaviour {
 				yield return null;
 			}
 		}
+	}
+
+	IEnumerator LoadNextTutorial(float time, string tutorialNumber)
+	{
+		Debug.Log("Start 10 Seconds");
+		yield return new WaitForSecondsRealtime(time);
+		Debug.Log("End 10 Seconds");
+
+		img.gameObject.SetActive(true);
+		for (float i = 0; i <= 1.1f; i += Time.deltaTime)
+		{
+			img.color = new Color(0, 0, 0, i);
+			yield return null;
+		}
+
+		SceneManager.LoadScene(tutorialNumber);
 	}
 }
