0f1c0d833c8281add3dff443160db8fcd0e7756b
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-01-26 16:56:54 +0900

    Tutorial + particle prefeb
    튜토리얼 작업 중

---
+++ 2019GGJ/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public Text nameText;
+	public Text dialogueText;
+
+	private Queue<string> sentences;
+	private bool coroutineEnd = true;
+	private string dialogue;
+	private string beforeSentence;
+
+	// Use this for initialization
+	void Start () {
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue(Dialogue dialogue)
+	{
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach(string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence()
+	{
+		if(sentences.Count==0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		if (coroutineEnd)
+		{
+			dialogue = sentences.Dequeue();
+			beforeSentence = dialogue;
+			StopAllCoroutines();
+			StartCoroutine(TypeSentence(dialogue)); //print dialogue typely
+		}
+		else
+		{
+			StopAllCoroutines();
+			dialogueText.text = beforeSentence; //print dialogue directly
+			coroutineEnd = true;
+		}
+
+	}
+
+	IEnumerator TypeSentence (string sentence)
+	{
+		coroutineEnd = false;
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+		coroutineEnd = true;
+
+	}
+
+	void EndDialogue()
+	{
+		Debug.Log("End Dialogue");
+	}
+}
---
+++ 2019GGJ/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+
+	public Dialogue dialogue;
+
+	void FixedUpdate()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("첫번째 터치");
+			TriggerDialogue();
+		}
+	}
+
+	public void TriggerDialogue()
+	{
+		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+	}
+}
