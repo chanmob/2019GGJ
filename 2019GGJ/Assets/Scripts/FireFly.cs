327aa71fbd2b6101634d66d523bf3ee4609cebf7
Chanmob <sellingout@naver.com>
2019-01-27 01:11:36 +0900

    반딧불이 맵에 추가

---
+++ 2019GGJ/Assets/Scripts/FireFly.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFly : MonoBehaviour
+{
+    public ParticleSystem[] ps;
+
+    private bool oneTime;
+
+    private void Start()
+    {
+        ps = GetComponentsInChildren<ParticleSystem>(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !oneTime)
+        {
+            oneTime = true;
+            ps[0].gameObject.SetActive(false);
+            ps[1].gameObject.SetActive(true);
+            StartCoroutine(Disappear());
+        }
+    }
+
+    private IEnumerator Disappear()
+    {
+        yield return new WaitForSeconds(5f);
+
+        this.gameObject.SetActive(false);
+    }
+}
