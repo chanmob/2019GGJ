33f4cb32127e4fa27dac27d543e208c6e0f638a3
Chanmob <sellingout@naver.com>
2019-01-27 14:07:44 +0900

    스테이지 세팅 창 전체 오디오 조절 추가

---
+++ 2019GGJ/Assets/Scripts/Sound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sound : MonoBehaviour
+{
+    public void AdjustSound(Slider slider)
+    {
+        AudioListener.volume = slider.value;
+    }
+}
